bf1d1ce34248d97a59bf822d93579796d7c2b0bf
Philip <jp.knirsch@yahoo.de>
2026-02-27 12:41:22 +0100

    Preprocessing

---
+++ dataverse_files/README.txt
@@ -0,0 +1,25 @@
+Latest version (v13, 19 April 2025) contains updated dataset including Session 79 (2024). 
+
+
+- The dataset now covers the United Nations General Debate Corpus (UNGDC) for the period 1946-2024.
+	"UNGDC_1946-2024.tgz"
+
+- There are 10,952 speeches in plain text format (UTF8). Speeches are structured by Year (Session). Each speech is named using the following convention: ISO 3166-1 alpha-3 country code, followed by the UN Session number, followed by year. E.g. USA_75_2020.txt.
+
+- For 2024, official transcripts of the UN General Debate with certified parallel translations into official UN languages were not yet available at the time of preparing this update. Instead, we relied on the official UN webpage for the 2024 General Debate (https://gadebate.un.org/en), which contains statements as delivered by each leader, with PDFs made available by country delegations. Most PDFs were machine-readable; however, several were image copies that required optical character recognition (OCR) processing using the Tesseract package in Python.
+Statements in languages other than English were translated using the OpenAI GPT-4o model with a professional translator prompt ("You are a professional translator at the UN, translate the following statement into English..."). A random sample of translations was validated using other translation systems (Google Translate) and using text alignment. Several countries had not yet made their statements available (Turkey, Hungary, Uruguay, and Liechtenstein). For these four countries, we utilised the English language soundtrack (MP3 file) of the official simultaneous translation, first transcribing it using the ElevenLabs speech-to-text system, and then translating it into English using OpenAI GPT-4o.
+
+- The collection also contains a file (Speakers_by_session.xlsx) recording names and posts of speakers in UN General Debates. Note: before 1994 the UN records do not consistently identify the posts of all speakers.
+
+- Original source files contain verbatim daily transcripts of the UN General Debate (and any other business on the UN agenda on the same day). Transcripts (in PDF format) were made available by the UN Library and processed to produce the UNGD Corpus as described in the paper. The original source files (PDFs) are in several tarballs: 
+	“Raw_PDFs_1946-1969.tgz”; 
+	“Raw_PDFs_1970-1990.tgz”; 
+	“Raw_PDFs_1991-2022.tgz”.    
+
+- When using the UNGDC data, please cite: 
+
+Slava Jankin, Alexander Baturo, and Niheer Dasandi. "Words to unite nations: The complete UN General Debate Corpus, 1946-present." Journal of Peace Research, forthcoming.
+
+AND 
+
+Alexander Baturo, Niheer Dasandi, and Slava Mikhaylov, "Understanding State Preferences With Text As Data: Introducing the UN General Debate Corpus" Research & Politics, 2017.
---
+++ topics_in_UN_general_debates.R
@@ -0,0 +1,109 @@
+# Topics in UN General debates
+
+options(stringsAsFactors = FALSE) 
+library(quanteda) 
+library(quanteda.textstats)
+library(tidyverse)
+library(dplyr)
+require(topicmodels)
+
+## Preprocessing
+# Da sich die Korpusdateien in einer Struktur mit einem Ordner pro Jahr und 
+# jeweils eine Datei pro Land befinden, muessen die Metadaten (Session, Jahr, Land) extrahiert werden.
+
+# In einem DataFrame werden die Dateipfade, welche die Metadaten enthalten, verwaltet
+file_paths <- list.files(path = "./dataverse_files/UN General Debate Corpus/TXT/", 
+                         pattern = "\\.txt$", 
+                         recursive = TRUE, 
+                         full.names = TRUE)
+data_frame <- tibble(filepath = file_paths) %>% 
+  mutate( #dplyr library
+    # Session (Sitzungsperiode), Jahr, Land und Rede extrahieren
+    session = str_extract(filepath, "\\d{2}"),
+    year = str_extract(filepath, "\\d{4}"),
+    country = str_extract(basename(filepath), "^[A-Z]{3}"),
+    speech = map_chr(filepath, ~ read_file(.x)),
+  ) %>%
+  # Filepath-Spalte nicht mehr benoetigt
+  select(session, year, country, speech)
+
+doc_names <- paste0(data_frame$year, data_frame$country)
+ungd_corpus <- corpus(data_frame$speech, docnames = doc_names) #quanteda
+
+
+# Lemma-Woerterbuch und stopword list lesen
+lemma_data <- read.csv("./baseform_en.tsv", encoding = "UTF-8")
+stopwords_extended <- readLines("./stopwords_en.txt", encoding = "UTF-8")
+
+# Tokenisierung #quanteda library
+corpus_tokens <- ungd_corpus %>%
+  tokens(remove_punct = TRUE, remove_numbers = TRUE, remove_symbols = TRUE) %>%
+  tokens_tolower() %>%
+  tokens_remove(pattern = stopwords_extended, padding = T) %>%
+  tokens_replace(lemma_data$inflected_form, lemma_data$lemma, valuetype = "fixed")
+  
+
+# Die Kollokationen einfach wie in der Uebung zu bilden ist hier nicht moeglich, da das Korpus viel zu gross ist
+#ungd_collocations <- quanteda.textstats::textstat_collocations(corpus_tokens, min_count = 25)
+#ungd_collocations <- ungd_collocations[1:250, ]
+#corpus_tokens <- tokens_compound(corpus_tokens, ungd_collocations)
+# -> Loesung: ngram-Erstellung; oder Kollokationen bilden auf Stichprobe (10% des Korpus)
+
+# ngrams
+ungd_bigrams <- tokens_ngrams(corpus_tokens, n = 2)
+
+# Dokument-Term-Matrix / document-feature matrix
+bigram_dfm <- ungd_bigrams %>%
+  tokens_remove("") %>%
+  dfm() %>%
+  dfm_trim(min_docfreq = 3)
+dim(bigram_dfm)
+
+top_phrases <- topfeatures(bigram_dfm, n = 250) 
+head(top_phrases, 40)
+top5_phrases <- c("unite_nation", "general_assembly", "international_community", "develop_country",
+                  "security_council")
+bigram_dfm <- bigram_dfm[, !(colnames(bigram_dfm) %in% top5_phrases)]
+
+# aus uebung:
+# due to vocabulary pruning, we have empty rows in our DTM # LDA does not like this. So we remove those docs from the # DTM and the metadata
+#sel_idx <- rowSums(DTM) > 0
+#DTM <- DTM[sel_idx, ]
+#textdata <- textdata[sel_idx, ]
+
+sel_idx <- rowSums(bigram_dfm) > 0
+bigram_dfm <- bigram_dfm[sel_idx, ]
+data_frame <- data_frame[sel_idx, ]
+
+
+# topic modelling
+K <- 20 # Anzahl Topics
+topicModel <- LDA(bigram_dfm, K, method = "Gibbs", control = list(
+  iter = 500,
+  seed = 1,
+  verbose = 25,
+  alpha = 0.02
+))
+tmResult <- posterior(topicModel)
+attributes(tmResult)
+#ncol(bigram_dfm) # lengthOfVocab
+# topics are probability distribtions over the entire # vocabulary
+#beta <- tmResult$terms # get beta from results 
+#dim(beta) # K distributions over ncol(DTM) terms
+#rowSums(beta) # rows in beta sum to 1
+#nrow(bigram_dfm) # size of collection
+# for every document we have a probability distribution of # its contained topics
+#theta <- tmResult$topics
+#dim(theta) # nDocs(DTM) distributions over K topics
+#rowSums(theta)[1:10] # rows in theta sum to 1
+
+terms(topicModel, 10)
+#top5termsPerTopic <- terms(topicModel, 5)
+#topicNames <- apply(top5termsPerTopic, 2, paste, collapse = " ")
+
+# ich weiss nicht, ob es der richtige Weg ist, nur Bigramme zu vewenden!?!?!?!?
+
+
+# Visualisierung
+
+
